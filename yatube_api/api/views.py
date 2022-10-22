d06e9289534515e1b5732d7d493c8cf78c87120f
Oleg <pivkin.owow@yandex.ru>
2022-10-22 18:38:56 +0300

    first commit

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,61 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, permissions, filters, mixins
+from rest_framework.pagination import LimitOffsetPagination
+
+from .permissions import IsAuthorOrReadOnly
+from posts.models import Post, Group
+from .serializers import (FollowSerializer, PostSerializer, GroupSerializer,
+                          CommentSerializer, FollowSerializer)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """API для работы с комментариями."""
+
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsAuthorOrReadOnly]
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        return post.comments.all()
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user, post=post)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    """API для работы с группами."""
+
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """API для работы с постами."""
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsAuthorOrReadOnly]
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class FollowViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
+    """API для работы с подписками."""
+
+    serializer_class = FollowSerializer
+    permission_classes = [permissions.IsAuthenticated,]
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('following__username',)
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.follower.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
